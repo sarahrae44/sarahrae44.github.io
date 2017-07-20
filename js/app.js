ceb322e5df3aa444ffe2c2a1f7179e572f48e93b
Sarah Anderson <sarahanderson@MacBook-Pro.local>
2017-07-20 13:57:20 -0600

    two player scoring working

--- js/app.js
+++ js/app.js
@@ -1,208 +1,164 @@
 $(() => {
 
+  // let playerOneScore = 0;
+  // let playerTwoScore = 0;
+  // // if decide to use
+  // // let time = 30;
+  let round = 1;
+  let playerOneName = '';
+  let playerTwoName = '';
+
   let playerOneScore = 0;
   let playerTwoScore = 0;
-  // if decide to use
-  // let time = 30;
-  let round = 1;
-  let modalName = '';
-  let modalNameTwo = '';
-  //
-  // array for holding cards that have already been played
-  // let cardsPlayed = [];
-  // let firstCardFlipped = [];
-  // let secondCardFlipped = [];
 
-  // instructions - will pop up when page is loaded; will have either an okay button or close functionality
+  let cardsInPlay = [];
 
-  // $('#instructionsButton').on('click', (e) => {
-  //   modalName = $('input').val()
-  //   // e.currentTarget is button
-  //   $(e.currentTarget).parent().css('display', 'none');
-  //   showWelcomeMessage();
-  // })
+  let turn = 1;
 
+  let roundOneCards = [
+    {
+      cardImage: 'images/erlich.png'
+    },
+    {
+      cardImage: 'images/erlich2.png'
+    },
+    {
+      cardImage: 'images/richard.png'
+    },
+    {
+      cardImage: 'images/siliconValleyGroup.png'
+    },
+    {
+      cardImage: 'images/siliconValleyLogo.png'
+    },
+    {
+      cardImage: 'images/svGroup.png'
+    },
+    {
+      cardImage: 'images/erlich.png'
+    },
+    {
+      cardImage: 'images/erlich2.png'
+    },
+    {
+      cardImage: 'images/richard.png'
+    },
+    {
+      cardImage: 'images/siliconValleyGroup.png'
+    },
+    {
+      cardImage: 'images/siliconValleyLogo.png'
+    },
+    {
+      cardImage: 'images/svGroup.png'
+    }
+  ];
 
+  roundOneCards.sort(() => Math.random() * 2 - 1);
 
-
-
-// ************************************************************************
-let cards = [
-  {
-    cardImage: 'images/erlich.png'
-  },
-  {
-    cardImage: 'images/erlich2.png'
-  },
-  {
-    cardImage: 'images/richard.png'
-  },
-  {
-    cardImage: 'images/siliconValleyGroup.png'
-  },
-  {
-    cardImage: 'images/siliconValleyLogo.png'
-  },
-  {
-    cardImage: 'images/svGroup.png'
-  },
-  {
-    cardImage: 'images/erlich.png'
-  },
-  {
-    cardImage: 'images/erlich2.png'
-  },
-  {
-    cardImage: 'images/richard.png'
-  },
-  {
-    cardImage: 'images/siliconValleyGroup.png'
-  },
-  {
-    cardImage: 'images/siliconValleyLogo.png'
-  },
-  {
-    cardImage: 'images/svGroup.png'
-  }
-];
-
-//cardsInPlay array, empty awaiting cards to be played
-let cardsInPlay = [];
-
-cards.sort(() => Math.random() * 2 - 1);
-
-
-
-// *************************************************************
-
-$('#modalButton').on('click', (e) => {
-    modalName = $('#playerOne').val()
-    // e.currentTarget is button
-    $(e.currentTarget).parent().css('display', 'none');
-    showWelcomeMessage();
-    console.log(modalName);
-  })
-
-
-  $('#modalButtonTwo').on('click', (e) => {
-      modalNameTwo = $('#playerTwo').val()
+  $('#modalButton').on('click', (e) => {
+      playerOneName = $('#playerOne').val()
       // e.currentTarget is button
       $(e.currentTarget).parent().css('display', 'none');
-      showWelcomeMessageTwo();
+      updateScoreOne();
+    })
+
+  $('#modalButtonTwo').on('click', (e) => {
+      playerTwoName = $('#playerTwo').val()
+      // e.currentTarget is button
+      $(e.currentTarget).parent().css('display', 'none');
+      updateScoreTwo();
       createBoard();
     })
 
+    const updateScoreOne = () => {
+      $('#playerOneScore').text('Player One ' + '(' + playerOneName + ') ' + 'Score: ' + playerOneScore);
+    }
 
-// displays a message;
-  const showWelcomeMessage = () => {
-    const $messageDiv = $('<div/>').text('Player one: ' + modalName);
-    $('header').append($messageDiv)
-  }
+    const updateScoreTwo = () => {
+      $('#playerTwoScore').text('Player Two ' + '(' + playerTwoName + ') ' + 'Score: ' + playerTwoScore);
+    }
 
-  const showWelcomeMessageTwo = () => {
-    const $messageTwoDiv = $('<div/>').text('Player two: ' + modalNameTwo);
-    $('header').append($messageTwoDiv)
-  }
-
-// ****************************************************************************
-
-//checkForMatch function, if the 1st and 2nd cards played are the same (from the cards array), confirm match.  If not, alert otherwise.  Successful match increases score.
-const checkForMatch = function() {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    playerScore +=1;
-    currentScore.innerHTML = playerScore;
-    alert('You found a match!');
-    //turn card opacity to 0
-  } else {
-    alert('Sorry, try again.');
-    // turn card back over
-  };
-};
-
-//flipCard function, turns card over to show 'face' by changing the image src upon click.  this = card that has been clicked (event listener in createBoard function).
-
-const flipCard = function() {
-  let cardId = this.getAttribute('data-id');
-  cardsInPlay.push(cards[cardId].cardImage);
-  this.setAttribute('src', cards[cardId].cardImage);
-    if (cardsInPlay.length === 2) {
-      checkForMatch();
-      cardsInPlay = []; //clears cards in play after match check
+    const checkForMatch = function() {
+      if (cardsInPlay[0] === cardsInPlay[1]) {
+        // make modal
+        // alert('You found a match!');
+        // change opacity of cards in cardsInPlay array
+          if (turn % 2 === 0){
+          playerTwoScore++;
+          updateScoreTwo();
+        }else{
+          playerOneScore++;
+          updateScoreOne();
+        }
+      } else {
+        //make modal
+        // alert('Sorry, try again.');
+        // flip cards back over
+        // flipCardBack();
+      };
     };
-};
-
-// start of game
-const createBoard = function() {
-  for (let i = 0; i < cards.length; i++) {
-    let cardElement = document.createElement('img');
-    cardElement.setAttribute('src', 'images/cardBack_130.png');
-    cardElement.setAttribute('data-id', i);
-    cardElement.addEventListener('click', flipCard);
-    let gameBoard = document.getElementById('game-board');
-    gameBoard.appendChild(cardElement);
-  };
-};
-
-
-// ********************************************************************
-
-//
-//   // score board
-//
-// const updateScore = () => {
-//   $('h1').text('scoreboard: ' + score);
-// }
-//
-// // round logic
-//
-// const setUpRound = () => {
-//   $('.squares').empty();
-//   $('round').text('round: '+ round);
-//   if(round === 1){
-//     createSquares(12);
-//     time = 30;
-//   }else if(round === 2){
-//     createSquares(24);
-//     time = 20;
-//   }else if(round === 3){
-//     createSquares(36);
-//     time = 20;
-//   }else{
-//     createSquares(48);
-//     time = 10;
-//   }
-// }
-//
-// // when zero cards left (all are set to opacity:0), move to next round
-// // round++
-// // display score modal
-//
-// // start game
-//
-//   $("#resetButton").on("click", (e) => {
-//     setUpRound();
-//     // setTimer();
-//   })
 
 
 
+    const flipCard = function() {
+      let cardId = this.getAttribute('data-id');
+      // console.log('User flipped ' + cards[cardId].rank + ' of ' + cards[cardId].suit + ', image: ' + cards[cardId].cardImage);
+      cardsInPlay.push(roundOneCards[cardId].cardImage);
+      this.setAttribute('src', roundOneCards[cardId].cardImage);
+        if (cardsInPlay.length === 2) {
+          checkForMatch();
+          turn+=1;
+          cardsInPlay = []; //clears cards in play after match check
+        };
+    };
 
-})
+    // const flipCardBack = function() {
+    //   for (let i = 0; i < roundOneCards.length; i++) {
+    //     let cardBack = document.createElement('img');
+    //     cardBack.setAttribute('src', 'images/cardBack_130.png');
+    //     cardBack.setAttribute('data-id', i);
+    //     cardBack.addEventListener('click', flipCard);
+    //     let gameBoard = document.getElementById('game-board');
+    //     gameBoard.appendChild(cardBack);
+    //   }
+    // }
 
+    const createBoard = function() {
+      for (let i = 0; i < roundOneCards.length; i++) {
+        let cardElement = document.createElement('img');
+        cardElement.setAttribute('src', 'images/cardBack_130.png');
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        let gameBoard = document.getElementById('game-board');
+        gameBoard.appendChild(cardElement);
+      };
+    };
 
+    const setUpRound = () => {
+      $('.squares').empty();
+      $('round').text('round: '+ round);
+      if(round === 1){
+        createSquares(12);
+        time = 30;
+      }else if(round === 2){
+        createSquares(24);
+        time = 20;
+      }else if(round === 3){
+        createSquares(36);
+        time = 20;
+      }else{
+        createSquares(48);
+        time = 10;
+      }
+    }
+
+      $("#resetButton").on("click", (e) => {
+        setUpRound();
+        // setTimer();
+      })
 
 
 
 
-
-
-// game over logic
-
-
-// optional win animation
-
-
-// optional timer functionality
-
-
-// reset board
+  })
