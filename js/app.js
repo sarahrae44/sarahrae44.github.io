74bbc2aef1038d8611b1fade2e5416857b241ce0
Sarah Anderson <sarahanderson@MacBook-Pro.local>
2017-07-21 14:19:38 -0600

    two rounds functionalgit add -A

--- js/app.js
+++ js/app.js
@@ -1,16 +1,21 @@
 $(() => {
 
-  let round = 1;
-  let playerOneName = '';
-  let playerTwoName = '';
+// ******** global for whole game ********** //
 
+// round count
+  let round = 1;
+
+// player scores
   let playerOneScore = 0;
   let playerTwoScore = 0;
 
+// pair of cards picked in each turn
   let cardsInPlay = [];
 
+// used to determine whose turn it is
   let turn = 1;
 
+// cards for round one
   let roundOneCards = [
     {
       cardImage: 'images/erlich.png'
@@ -50,123 +55,137 @@ $(() => {
     }
   ];
 
+// cards for round two
   let roundTwoCards = [
     {
-      cardImage: 'images/erlich.png'
+      cardImage: 'images/bobs.png'
     },
     {
-      cardImage: 'images/erlich2.png'
+      cardImage: 'images/chotchkiesWaiter.png'
     },
     {
-      cardImage: 'images/richard.png'
+      cardImage: 'images/copyMachineMassacre.png'
     },
     {
-      cardImage: 'images/siliconValleyGroup.png'
+      cardImage: 'images/drew.png'
     },
     {
-      cardImage: 'images/siliconValleyLogo.png'
+      cardImage: 'images/lawrence.png'
     },
     {
-      cardImage: 'images/svGroup.png'
+      cardImage: 'images/lumbergMug.png'
     },
     {
-      cardImage: 'images/erlich.png'
+      cardImage: 'images/michaelAtComputer.png'
     },
     {
-      cardImage: 'images/erlich2.png'
+      cardImage: 'images/miltonAtDesk.png'
     },
     {
-      cardImage: 'images/richard.png'
+      cardImage: 'images/miltonInBasement.png'
     },
     {
-      cardImage: 'images/siliconValleyGroup.png'
+      cardImage: 'images/peter.png'
     },
     {
-      cardImage: 'images/siliconValleyLogo.png'
+      cardImage: 'images/samirAndMichael.png'
     },
     {
-      cardImage: 'images/svGroup.png'
+      cardImage: 'images/samirPrinter.png'
     },
     {
-      cardImage: 'images/erlich.png'
+      cardImage: 'images/bobs.png'
     },
     {
-      cardImage: 'images/erlich2.png'
+      cardImage: 'images/chotchkiesWaiter.png'
     },
     {
-      cardImage: 'images/richard.png'
+      cardImage: 'images/copyMachineMassacre.png'
     },
     {
-      cardImage: 'images/siliconValleyGroup.png'
+      cardImage: 'images/drew.png'
     },
     {
-      cardImage: 'images/siliconValleyLogo.png'
+      cardImage: 'images/lawrence.png'
     },
     {
-      cardImage: 'images/svGroup.png'
+      cardImage: 'images/lumbergMug.png'
     },
     {
-      cardImage: 'images/erlich.png'
+      cardImage: 'images/michaelAtComputer.png'
     },
     {
-      cardImage: 'images/erlich2.png'
+      cardImage: 'images/miltonAtDesk.png'
     },
     {
-      cardImage: 'images/richard.png'
+      cardImage: 'images/miltonInBasement.png'
     },
     {
-      cardImage: 'images/siliconValleyGroup.png'
+      cardImage: 'images/peter.png'
     },
     {
-      cardImage: 'images/siliconValleyLogo.png'
+      cardImage: 'images/samirAndMichael.png'
     },
     {
-      cardImage: 'images/svGroup.png'
+      cardImage: 'images/samirPrinter.png'
     }
   ];
 
+// sort functions for all rounds
   roundOneCards.sort(() => Math.random() * 2 - 1);
   roundTwoCards.sort(() => Math.random() * 2 - 1);
 
+// checks for which board to play
 const checkForRound = () => {
-  console.log(playerOneScore + playerTwoScore);
+
+// check to see if all possible points have been score for this round
   if(roundOneCards.length / (playerOneScore + playerTwoScore) === 2){
       round ++;
+    // set a variable to hold cards on board and clear them since game is over
       let clearGameBoard = document.getElementById('game-board');
         while (clearGameBoard.firstChild) {
         clearGameBoard.removeChild(clearGameBoard.firstChild);
       }
-      console.log(clearGameBoard);
+// set up next round
       setUpRound();
-  }
-  // console.log(round);
+    }
 }
 
+// player one name prompt
   $('#modalButton').on('click', (e) => {
+    // pass in entered value for player one name
       playerOneName = $('#playerOne').val()
       // e.currentTarget is button
       $(e.currentTarget).parent().css('display', 'none');
+      // update the scoring function
       updateScoreOne();
     })
 
+//player two name prompt
   $('#modalButtonTwo').on('click', (e) => {
+    // pass in entered value for player two name
       playerTwoName = $('#playerTwo').val()
       // e.currentTarget is button
       $(e.currentTarget).parent().css('display', 'none');
+      // update the scoring function
       updateScoreTwo();
+      // start game
       if(round === 1){
         createBoardOne();
       }
     })
 
+// player one update score function
     const updateScoreOne = () => {
       $('#playerOneScore').text('Player One ' + '(' + playerOneName + ') ' + 'Score: ' + playerOneScore);
     }
 
+// player two update score function
     const updateScoreTwo = () => {
       $('#playerTwoScore').text('Player Two ' + '(' + playerTwoName + ') ' + 'Score: ' + playerTwoScore);
     }
 
+// determines who gets to go
     const whoseTurn = () => {
       if(turn % 2 === 0){
         playerTwoTurn();
@@ -175,101 +194,219 @@ const checkForRound = () => {
       }
     }
 
+// highlights player two if their turn
     const playerTwoTurn = () => {
       $('#playerTwoScore').css('color', 'white');
       $('#playerOneScore').css('color', 'black');
     }
 
+// highlights player one if their turn
     const playerOneTurn = () => {
       $('#playerOneScore').css('color', 'white');
       $('#playerTwoScore').css('color', 'black');
     }
 
+// ************ end of global for whole game ********** //
+
+// ************ round one-specific ******************* //
+
+
     const testingTimer = () => {
       // this is when the cards should flip back
       // console.log('%cthe timer is working', 'color: green');
+      // create new class to hold the cards that were selected
       let selectedClass = document.getElementsByClassName('selected');
       // console.log(selectedClass);
       // console.log('in timer function');
 
-      // where I want to add that image is being flipped back; add image to selected class
-      selectedClass[0].setAttribute('src', 'images/cardBack_130.png');
-      selectedClass[1].setAttribute('src', 'images/cardBack_130.png');
+      // image is being flipped back; add card back image to selected class
+      selectedClass[0].setAttribute('src', 'images/piedPiperCardBack.png');
+      selectedClass[1].setAttribute('src', 'images/piedPiperCardBack.png');
       // need to remove class from images
       // console.log('selected');
+      // remove selected class so that cards can go back in game
       selectedClass[1].classList.remove('selected');
       // console.log('selected');
+      // remove selected class so that cards can go back in game
       selectedClass[0].classList.remove('selected');
       // console.log(selectedClass);
     }
 
     const flipCard = function(e) {
+      // determine which player is playing
       whoseTurn();
+      // get the id of the card flipped
       let cardId = this.getAttribute('data-id');
       // this proves that e.currentTarget works
       // e.currentTarget.style.visibility = "hidden";
+      // add the flipped cards to selected class
       e.currentTarget.classList.add('selected');
       // console.log(e.currentTarget);
       // console.log(cardId);
       // e.currentTarget.classList('selected');
+      // push selected cards into array so they can be compared
       cardsInPlay.push(roundOneCards[cardId].cardImage);
-
+      // list the src so logic can run to determine if they are the same
       this.setAttribute('src', roundOneCards[cardId].cardImage);
-
+      // once cardsInPlay array holds 2 cards, compare them to determine win/loss for turn
       if (cardsInPlay.length === 2) {
+        // if cards in array are the same
         if (cardsInPlay[0] === cardsInPlay[1]){
         // console.log(document.getElementsByClassName('selected'));
+        // create new variable for the matched cards to be added to, so the selected id gets removed from them
         let selectedClassMatch = document.getElementsByClassName('selected');
         // console.log(selectedClassMatch);
+        // remove selected class from the cards that match
         selectedClassMatch[1].classList.remove('selected');
         // console.log(selectedClassMatch);
+        // remove selected class from the cards that match
         selectedClassMatch[0].classList.remove('selected');
         // console.log(selectedClassMatch);
+        // check to see which player should receive point
           if (turn % 2 === 0){
             playerTwoScore++;
             updateScoreTwo();
+            checkForRound();
           }else{
             playerOneScore++;
             updateScoreOne();
+            checkForRound();
           }
         }else{
-          setTimeout(testingTimer, 1200);
+          // if cards don't match, faces of cards need to display for one second before being flipped back
+          setTimeout(testingTimer, 1000);
           // console.log('not in timer function');
         };
+        // add one to the turn variable to keep track of which player gets to go next
         turn+=1;
           //clears cards in play after match check
         cardsInPlay = [];
       };
-      checkForRound();
     };
 
+// set up board one
     const createBoardOne = () => {
+// count number in cards array and add loop through the following function that many times
       for (let i = 0; i < roundOneCards.length; i++) {
+// create a new element to hold cardElement
         let cardElement = document.createElement('img');
-        cardElement.setAttribute('src', 'images/cardBack_130.png');
+// image to be applied to element
+        cardElement.setAttribute('src', 'images/piedPiperCardBack.png');
+// assigns an id to each instance
         cardElement.setAttribute('data-id', i);
+// when object is clicked, call the flipCard function
         cardElement.addEventListener('click', flipCard);
+// set variable for game board
         let gameBoard = document.getElementById('game-board');
+// add the objects to the game board
         gameBoard.appendChild(cardElement);
       };
     };
 
+// ************** end of round one-specific ***************** //
+
+// ************** round two-specific ************************ //
+    const testingTimerTwo = () => {
+      // this is when the cards should flip back
+      // console.log('%cthe timer is working', 'color: green');
+      // create new class to hold the cards that were selected
+      let selectedClass = document.getElementsByClassName('selected');
+      // console.log(selectedClass);
+      // console.log('in timer function');
+
+      // image is being flipped back; add card back image to selected class
+      selectedClass[0].setAttribute('src', 'images/officeSpaceCardBack.png');
+      selectedClass[1].setAttribute('src', 'images/officeSpaceCardBack.png');
+      // need to remove class from images
+      // console.log('selected');
+      // remove selected class so that cards can go back in game
+      selectedClass[1].classList.remove('selected');
+      // console.log('selected');
+      // remove selected class so that cards can go back in game
+      selectedClass[0].classList.remove('selected');
+      // console.log(selectedClass);
+    }
+
+
+    const flipCardTwo = function(e) {
+      // determine which player is playing
+      whoseTurn();
+      // get the id of the card flipped
+      let cardId = this.getAttribute('data-id');
+      // this proves that e.currentTarget works
+      // e.currentTarget.style.visibility = "hidden";
+      // add the flipped cards to selected class
+      e.currentTarget.classList.add('selected');
+      // console.log(e.currentTarget);
+      // console.log(cardId);
+      // e.currentTarget.classList('selected');
+      // push selected cards into array so they can be compared
+      cardsInPlay.push(roundTwoCards[cardId].cardImage);
+      // list the src so logic can run to determine if they are the same
+      this.setAttribute('src', roundTwoCards[cardId].cardImage);
+      // once cardsInPlay array holds 2 cards, compare them to determine win/loss for turn
+      if (cardsInPlay.length === 2) {
+        // if cards in array are the same
+        if (cardsInPlay[0] === cardsInPlay[1]){
+        // console.log(document.getElementsByClassName('selected'));
+        // create new variable for the matched cards to be added to, so the selected id gets removed from them
+        let selectedClassMatch = document.getElementsByClassName('selected');
+        // console.log(selectedClassMatch);
+        // remove selected class from the cards that match
+        selectedClassMatch[1].classList.remove('selected');
+        // console.log(selectedClassMatch);
+        // remove selected class from the cards that match
+        selectedClassMatch[0].classList.remove('selected');
+        // console.log(selectedClassMatch);
+        // check to see which player should receive point
+          if (turn % 2 === 0){
+            playerTwoScore++;
+            updateScoreTwo();
+            checkForRound();
+          }else{
+            playerOneScore++;
+            updateScoreOne();
+            checkForRound();
+          }
+        }else{
+          // if cards don't match, faces of cards need to display for one second before being flipped back
+          setTimeout(testingTimerTwo, 1200);
+          // console.log('not in timer function');
+        };
+        // add one to the turn variable to keep track of which player gets to go next
+        turn+=1;
+          //clears cards in play after match check
+        cardsInPlay = [];
+      };
+    };
+
+    // set up board one
     const createBoardTwo = () => {
+      // count number in cards array and add loop through the following function that many times
       for (let i = 0; i < roundTwoCards.length; i++) {
-        let cardElement = document.createElement('img');
-        cardElement.setAttribute('src', 'images/cardBack_130.png');
+        // create a new element to hold cardElement
+      let cardElement = document.createElement('img');
+      // image to be applied to element
+      cardElement.setAttribute('src', 'images/officeSpaceCardBack.png');
+      // assigns an id to each instance
         cardElement.setAttribute('data-id', i);
-        cardElement.addEventListener('click', flipCard);
-        let gameBoard = document.getElementById('game-board');
-        gameBoard.appendChild(cardElement);
+        // when object is clicked, call the flipCard function
+      cardElement.addEventListener('click', flipCardTwo);
+      // set variable for game board
+      let gameBoard = document.getElementById('game-board');
+      // add the objects to the game board
+      gameBoard.appendChild(cardElement);
       };
     };
 
+// **************** end of round two-specific *************** //
+
+// checks for round to set up
     const setUpRound = () => {
       // $('.squares').empty();
       // $('round').text('round: '+ round);
       if(round === 1){
-        createBoardTwo();
+        createBoardOne();
       }else if(round === 2){
         createBoardTwo();
       // createSquares(24);
